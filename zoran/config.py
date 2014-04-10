7297c523489d8b896b490552f1f7466f985577e5
Russell Sim <russell.sim@gmail.com>
2014-04-10 14:13:51 +1000

    Integrated email functionality

--- zoran/config.py
+++ zoran/config.py
@@ -2,29 +2,70 @@ from oslo.config import cfg
 
 CONF = cfg.CONF
 
+
+#
+#  Nova settings
+#
 nova_group = cfg.OptGroup(name='nova',
                           title='Nova Options')
 CONF.register_group(nova_group)
 
-nova_tenant_opt = cfg.StrOpt('admin_tenant_name',
-                             required=True,
-                             help='Tenant name')
+nova_tenant_opt = cfg.StrOpt(
+    'admin_tenant_name',
+    required=True,
+    help='Tenant name')
 CONF.register_opt(nova_tenant_opt, group=nova_group)
 
-nova_user_opt = cfg.StrOpt('admin_username',
-                           required=True,
-                           help='Openstack username')
+nova_user_opt = cfg.StrOpt(
+    'admin_username',
+    required=True,
+    help='Openstack username')
 CONF.register_opt(nova_user_opt, group=nova_group)
 
-nova_password_opt = cfg.StrOpt('admin_password',
-                               required=True,
-                               help='Password to connect Openstack with')
+nova_password_opt = cfg.StrOpt(
+    'admin_password',
+    required=True,
+    help='Password to connect Openstack with')
 CONF.register_opt(nova_password_opt, group=nova_group)
 
-nova_auth_url_opt = cfg.StrOpt('auth_url',
-                               required=True,
-                               help='Keystone URL')
+nova_auth_url_opt = cfg.StrOpt(
+    'auth_url',
+    required=True,
+    help='Keystone URL')
 CONF.register_opt(nova_auth_url_opt, group=nova_group)
+
+#
+#  Email settings
+#
+email_group = cfg.OptGroup(name='email',
+                           title='Email Options')
+CONF.register_group(email_group)
+
+email_report_subject_opt = cfg.StrOpt(
+    'report_subject',
+    required=True,
+    default='ZORAN the protector report',
+    help='Report email subject line.')
+CONF.register_opt(email_report_subject_opt, group=email_group)
+
+email_report_to_opt = cfg.StrOpt(
+    'report_to',
+    required=True,
+    help='Report to address')
+CONF.register_opt(email_report_to_opt, group=email_group)
+
+email_report_from_opt = cfg.StrOpt(
+    'report_from',
+    required=True,
+    help='Report from address')
+CONF.register_opt(email_report_from_opt, group=email_group)
+
+email_smtp_server_opt = cfg.StrOpt(
+    'smtp_server',
+    default='localhost',
+    required=True,
+    help='The hostname of the smtp server.')
+CONF.register_opt(email_smtp_server_opt, group=email_group)
 
 
 core_opts = [
